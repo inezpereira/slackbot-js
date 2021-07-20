2fea396a2a24a78b953bfbc1bba486aeed99b16a
Inês Pereira <pereira@biomed.ee.ethz.ch>
2021-07-20 15:02:16 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -11,21 +11,41 @@ rtm.start()
 
 rtm.on('ready', async () => {
     console.log('bot started')
-    sendMessage(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online.`)
+    sendMessage(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online. Send me a direct message with the text: "!start"`)
 })
 
 rtm.on('slack_event', async (eventType, event) => {
     if (event && event.type === 'message'){
-        if (event.text === '!hello') {
+        if (event.text === "!start") {
             hello(event.channel, event.user)
-        }
+        } else if (event.text === "!ready"){
+            start(event.channel)
+        } else if (event.text.toLowerCase() === "bayes"){
+            bayes(event.channel, event.user)
+    }
     }
 })
 
 
 function hello (channelId, userId) {
-    sendMessage(channelId, `Heya! <@${userId}>`)
+    sendMessage(channelId,`:tada: Heya <@${userId}>! Welcome to the CPC Zurich 2021 special social programme! :tada:
+In this challenge, you and your teammates will be given multiple puzzles to solve. This first team to solve all puzzles will win a special prize! :gift:
+Don't worry too much about instructions now: these will be provided along the way.
+Are you ready to start? If so, type "!ready"`)
+    // sendMessage(channelId, `:surfer: Check out this link: https://topia.io/cpc-game-free-0og8rqwlh <@${userId}>`)
 }
+
+function start (channelId) {
+    sendMessage(channelId, `Awesome. For your first challenge, find the secret puzzle in our surfers Topia room :surfer: : https://topia.io/cpc-game-free-0og8rqwlh
+Once you have found and solved the puzzle, type the solution into this chat.`)
+}
+
+function bayes (channelId){
+    sendMessage(channelId, `YES!! :notes: Because you know I'm all about about that Bayes, 'bout that Bayes, no trouble :notes:... What do you mean, that's not the actual lyrics? I don't care, I prefer this version :nerd_face:
+Anyway: congrats on making it through your first challenge! 
+Next... <insert challenge here @CPC devs>`)
+}
+
 
 async function sendMessage(channel, message) {
     await web.chat.postMessage({
@@ -33,3 +53,7 @@ async function sendMessage(channel, message) {
         text: message,
     })
 }
+
+// Type "yarn start" on your Terminal to run this.
+// Video tutorial: https://www.youtube.com/watch?v=AajBk59nOgw
+// How to create a classic App: https://api.slack.com/apps?new_classic_app=1
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const SLACK_OAUTH_TOKEN = 'Bot-User-OAuth-Access-Token'
-export const BOT_SPAM_CHANNEL = '#bot-spam' // this is the channel you want your bot online & spam to go
+export const SLACK_OAUTH_TOKEN = 'xoxb-2062124970819-2305432205121-g0vTpbFscAQWEFhVVJfp8Tv7'
+export const BOT_SPAM_CHANNEL = '#cpc-challenge' // this is the channel you want your bot online & spam to go
