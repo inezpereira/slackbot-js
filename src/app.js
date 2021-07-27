ff777cfb1ec81221e7e80cc30327cf42941e2a03
Inês Pereira <pereira@biomed.ee.ethz.ch>
2021-07-27 11:56:15 +0200

    First version of game

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ rtm.start()
 
 rtm.on('ready', async () => {
     console.log('bot started')
-    sendMessage(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online. Send me a direct message with the text: "!start"`)
+    sendMessage(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online. Send me a direct message with the text: !start`)
 })
 
 rtm.on('slack_event', async (eventType, event) => {
@@ -22,7 +22,13 @@ rtm.on('slack_event', async (eventType, event) => {
             start(event.channel)
         } else if (event.text.toLowerCase() === "bayes"){
             bayes(event.channel, event.user)
-    }
+        } else if (event.text.toLowerCase() === "ode"){
+            ode(event.channel, event.user)
+        } else if (event.text.toLowerCase() === "ijk"){
+            ijk(event.channel, event.user)
+        } else if (event.text.toLowerCase() === "anhedonia"){
+            anhedonia(event.channel)
+        }
     }
 })
 
@@ -31,7 +37,7 @@ function hello (channelId, userId) {
     sendMessage(channelId,`:tada: Heya <@${userId}>! Welcome to the CPC Zurich 2021 special social programme! :tada:
 In this challenge, you and your teammates will be given multiple puzzles to solve. This first team to solve all puzzles will win a special prize! :gift:
 Don't worry too much about instructions now: these will be provided along the way.
-Are you ready to start? If so, type "!ready"`)
+Are you ready to start? If so, type: !ready`)
     // sendMessage(channelId, `:surfer: Check out this link: https://topia.io/cpc-game-free-0og8rqwlh <@${userId}>`)
 }
 
@@ -41,11 +47,30 @@ Once you have found and solved the puzzle, type the solution into this chat.`)
 }
 
 function bayes (channelId){
-    sendMessage(channelId, `YES!! :notes: Because you know I'm all about about that Bayes, 'bout that Bayes, no trouble :notes:... What do you mean, that's not the actual lyrics? I don't care, I prefer this version :nerd_face:
+    sendMessage(channelId, `YES!! :notes: Because you know I'm all about about that Bayes, 'bout that Bayes, no trouble :notes:... What do you mean, that's not the actual lyrics? Hmm, I prefer this version :nerd_face:
 Anyway: congrats on making it through your first challenge! 
-Next... <insert challenge here @CPC devs>`)
+Next, go back to the Topia room and find the 20-faced die. The password is: frontier-psychiatrist
+Solve the riddle and type the solution into this chat.`)
 }
 
+function ode (channelId,userId){
+    sendMessage(channelId,`<@${userId}> strikes again! Next, what is equal to negative one? 
+(Hint: check out https://www.youtube.com/watch?v=SZXHoWwBcDc)
+Type the solution into this chat.`)
+}
+
+function ijk (channelId,userId){
+    sendMessage(channelId,`🎶 i-jay-kaaaaay 🎶 I hope you enjoyed this Science version of Lin-Manuel Miranda's "Alexander Hamilton". 
+For this next challenge, be sure to brainstorm with your team! Rebus puzzles are much more fun thay way :smiley:!
+Go back to the Topia room and find the canvas by the sea. The password is: quaternion-musical
+Once again, type the solution into this chat.`)
+}
+
+function anhedonia (channelId){
+    sendMessage(channelId,`You've made it! QUICK!! 
+Type your team number and copy and paste the passphrase into the #cpc-challenge and claim your prize!
+The passphrase is: Your body has run out of magnesium! 0Mg...`)
+}
 
 async function sendMessage(channel, message) {
     await web.chat.postMessage({
