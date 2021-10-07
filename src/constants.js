923558916285411241983d48c6087499f906d381
Inês Pereira <pereira@biomed.ee.ethz.ch>
2021-10-07 11:08:53 +0200

    Added warning

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const SLACK_OAUTH_TOKEN = '<enter-slack-token-here>'
+export const SLACK_OAUTH_TOKEN = '<enter-slack-token-here>' // never publish this information online!
 export const BOT_SPAM_CHANNEL = '#cpc-challenge-open' // this is the channel you want your bot online & spam to go
 export const BOT_MONITORING_CHANNEL = '#cpc-challenge-monitoring' // this is the channel to monitor responses
